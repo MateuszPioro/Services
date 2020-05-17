21eaf89e82ad422820070dd26f26e794bbe159c8
Mateusz Piórowicz <marian@@>
2020-05-17 22:27:42 +0200

    Initial commit

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,16 @@
+package io.github.mat3e;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        var logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World");
+
+    }
+}
